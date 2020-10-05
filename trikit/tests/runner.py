aca9af8c74dc405856c8ed6693558758adcdac6f
jtriv <james.triveri@gmail.com>
2020-10-04 21:50:18 -0500

    JDT: Removed various tests scripts.

--- trikit/tests/runner.py
+++ trikit/tests/runner.py
@@ -1,26 +1,7 @@
-"""
-
-Methods
-
-assertEqual(a, b)
-assertNotEqual(a, b)
-assertTrue(x)
-assertFalse(x)
-assertIs(a, b)
-assertIsNot(a, b)
-assertIsNone(x)
-assertIsNotNone(x)
-assertIn(a, b)
-assertNotIn(a, b)
-assertIsInstance(a, b)
-assertNotIsInstance(a, b)
-"""
 import unittest
-
 import triangle_ut
 import chainladder_ut
 import utils_ut
-
 
 loader = unittest.TestLoader()
 suite  = unittest.TestSuite()
