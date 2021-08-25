4320566908a34dd7bc07703f634be7e660c6bef0
jtriv <james.triveri@gmail.com>
2021-08-25 17:01:24 -0500

    JDT: WIP commit.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,8 @@ import sphinx_rtd_theme
 
 
 sys.path.insert(0, os.path.abspath("../../trikit"))
-# sys.path.insert(0, os.path.abspath("../../.."))
+sys.path.insert(0, os.path.abspath("../../.."))
+sys.path.insert(0, os.path.abspath("../.."))
 
 # -- General configuration ------------------------------------------------
 
